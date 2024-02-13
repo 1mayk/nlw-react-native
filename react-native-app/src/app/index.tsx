3184b0a807765804f942a7df8e11ab84ba52a008
Mayk <maycon.lrgo@gmail.com>
2024-02-13 02:02:43 -0300

    feat: instalando e configurando nativewind

--- react-native-app/src/app/index.tsx
+++ react-native-app/src/app/index.tsx
@@ -1,17 +1,9 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text } from 'react-native'
 
 export default function Home() {
   return (
-    <View style={styles.container}>
+    <View>
       <Text>Testando</Text>
     </View>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "red",
-    flex: 1,
-    alignItems: "center"
-  }
-})
