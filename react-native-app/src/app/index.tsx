eabf08c8bebbf07c25a1a5830ee7aff41b54a867
Mayk <maycon.lrgo@gmail.com>
2024-02-14 11:13:08 -0300

    feat: insere flatList de categorias

--- react-native-app/src/app/index.tsx
+++ react-native-app/src/app/index.tsx
@@ -1,15 +1,35 @@
-import { View, Text } from "react-native";
+import { useState } from "react";
+import { View, FlatList } from "react-native";
 import { Header } from "@/components/header";
 import { CategoryButton } from "@/components/category-button";
+import { CATEGORIES } from "@/utils/data/products";
 
 export default function Home() {
+  const [category, setCategory] = useState<string>(CATEGORIES[0]);
+
+  const handleCategorySelected = (item: string) => {
+    setCategory(item);
+  };
+
   return (
     <View className="flex-1 pt-8">
       <Header title="Faça seu pedido" cartQuantityItems={7} />
 
-      <View className="flex-row flex-wrap gap-3">
-        <CategoryButton title="Lanche do dia" isSelected/>
-      </View>
+      <FlatList
+        data={CATEGORIES}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <CategoryButton
+            title={item}
+            isSelected={item === category}
+            onPress={() => handleCategorySelected(item)}
+          />
+        )}
+        horizontal
+        className="max-h-10 mt-5"
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
+      />
     </View>
   );
 }
