f3c608ccd25b5762ee842156461fe8635994383c
Mayk <maycon.lrgo@gmail.com>
2024-02-17 19:25:17 -0300

    feat: add session list de produtos + card de produto inicial

--- react-native-app/src/app/index.tsx
+++ react-native-app/src/app/index.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { View, FlatList, SectionList, Text } from "react-native";
 import { Header } from "@/components/header";
 import { CategoryButton } from "@/components/category-button";
+import { Product } from "@/components/product";
 import { CATEGORIES, MENU } from "@/utils/data/products";
 
 export default function Home() {
@@ -33,20 +34,17 @@ export default function Home() {
 
       <SectionList
         sections={MENU}
-        // ele considera automaticamente esse "item" como cada campo "data" do
-        // objeto principal do array
-        // TODAS AS POSIÇÕES PRECISAM TER A MESMA TIPAGEM?
         keyExtractor={(item) => item.id}
-        // Garantir uma visualização melhor, onde a lista não se estica e nem
-        // fica em cima de outros produtos
         stickySectionHeadersEnabled={false}
-        // Informação que irei renderizar
-        renderItem={({ item }) => <Text>{item.title}</Text>}
-        // header da seção?? (ver documentação)
-        // exibe o title, a categoria da sessão
-        // isso ocorre por que essa lista é específica dessa forma
-        // separando cada posição do objeto principal do array como uma session
-        renderSectionHeader={({ section: { title } }) => <Text>{title}</Text>}
+        renderItem={({ item }) => <Product data={item} />}
+        renderSectionHeader={({ section: { title } }) => (
+          <Text className="text-xl text-white font-heading mt-8 mb-3">
+            {title}
+          </Text>
+        )}
+        className="flex-1 p-5"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
       />
     </View>
   );
