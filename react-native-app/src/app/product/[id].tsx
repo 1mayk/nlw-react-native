8e0b0ed2eb3173c26667031390c4c4c1f5fe00fe
Mayk <maycon.lrgo@gmail.com>
2024-02-25 19:17:31 -0300

    feat: add título ao product details

--- react-native-app/src/app/product/[id].tsx
+++ react-native-app/src/app/product/[id].tsx
@@ -1,7 +1,7 @@
 import { View, Text, Image, ScrollView } from "react-native";
 import { useLocalSearchParams, useNavigation } from "expo-router";
 import { Feather } from "@expo/vector-icons";
-
+import { Redirect } from "expo-router";
 import { useCartStore } from "@/stores/cart-store";
 
 import { PRODUCTS } from "@/utils/data/products";
@@ -14,11 +14,15 @@ export default function Product() {
   const navigation = useNavigation();
   const { id } = useLocalSearchParams();
 
-  const product = PRODUCTS.filter((item) => item.id === id)[0];
+  const product = PRODUCTS.find((item) => item.id === id);
 
   function handleAddCart() {
-    cartStore.add(product);
+    cartStore.add(product!);
     navigation.goBack();
+  }
+
+  if (!product) {
+    return <Redirect href="/" />;
   }
 
   return (
@@ -33,6 +37,9 @@ export default function Product() {
 
         {/* Price + Description + Ingredients */}
         <View className="p-5 mt-8 flex-1">
+          <Text className="text-white text-xl font-heading">
+            {product.title}
+          </Text>
           <Text className="text-lime-400 text-2xl font-heading my-2">
             {formatCurrency(product.price)}
           </Text>
